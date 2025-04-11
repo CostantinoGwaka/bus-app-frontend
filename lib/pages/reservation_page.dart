df27a30a1eec091e5ca784b21ab782f8ae1046c1
Costantino Dionis Gwaka <gwaka94@gmail.com>
2025-04-11 19:29:46 +0300

    fix

---
+++ lib/pages/reservation_page.dart
@@ -0,0 +1,96 @@
+import 'package:app/model/reservation_expansion_item.dart';
+import 'package:app/provider/app_data_provider.dart';
+import 'package:app/utils/helper_functions.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '../customwidgets/reservation_item_body_view.dart';
+import '../customwidgets/reservation_item_header_view.dart';
+import '../customwidgets/search_box_view.dart';
+
+class ReservationPage extends StatefulWidget {
+  const ReservationPage({super.key});
+
+  @override
+  State<ReservationPage> createState() => _ReservationPageState();
+}
+
+class _ReservationPageState extends State<ReservationPage> {
+  List<ReservationExpansionItem> items = [];
+  bool isFirst = true;
+  @override
+  void didChangeDependencies() {
+    if (isFirst) {
+      _getData();
+    }
+    super.didChangeDependencies();
+  }
+
+  _getData() async {
+    final appDataProvider = Provider.of<AppDataProvider>(
+      context,
+      listen: false,
+    );
+    final reservations = await appDataProvider.getAllReservations();
+    setState(() {
+      items = appDataProvider.getExapansionItems(reservations);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Reservation List')),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            SearchBoxView(
+              onSubmit: (value) {
+                _search(value);
+              },
+            ),
+            ExpansionPanelList(
+              expansionCallback: (index, isExpanded) {
+                setState(() {
+                  items[index].isExpanded = !isExpanded;
+                });
+              },
+              children:
+                  items
+                      .map(
+                        (item) => ExpansionPanel(
+                          isExpanded: item.isExpanded,
+                          headerBuilder:
+                              (context, isExpanded) =>
+                                  ReservationItemHeaderView(
+                                    header: item.header,
+                                  ),
+                          body: ReservationItemBodyView(body: item.body),
+                        ),
+                      )
+                      .toList(),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  void _search(String value) async {
+    final data = await Provider.of<AppDataProvider>(
+      context,
+      listen: false,
+    ).getReservationsByMobile(value);
+    if (data.isEmpty) {
+      // ignore: use_build_context_synchronously
+      showMsg(context, 'No record found');
+      return;
+    }
+    setState(() {
+      items = Provider.of<AppDataProvider>(
+        context,
+        listen: false,
+      ).getExapansionItems(data);
+    });
+  }
+}
